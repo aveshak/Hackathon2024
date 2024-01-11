ba0d5c5a56124e503ddc8e0954937f3d79993424
Avesh.Kumar <avesh.kumar@gomomentus.com>
2024-01-11 20:33:16 +0530

    add model for Event and Account

---
+++ EnterpriseODataApis/Models/Account.cs
@@ -0,0 +1,63 @@
+﻿namespace EnterpriseODataApis.Models
+{
+    [Serializable]
+    [Table("Account")]
+    public class Account
+    {
+        [Key]
+        [Column("company_id")]
+        public int Id { get; set; }
+        [Column("org_srl_id")]
+        public string Organization { get; set; }
+        [Column("acc_first_name")]
+        public string? FirstName { get; set; }
+        [Column("acc_last_name")]
+        public string? LastName { get; set; }
+        [Column("acc_name")]
+        public string? Name { get; set; }
+        [Column("acc_saluation")]
+        public string? Salutation { get; set; }
+        [Column("acc_title")]
+        public string? Title { get; set; }
+        [Column("acc_address_l1")]
+        public string? Address1 { get; set; }
+        [Column("acc_address_l2")]
+        public string? Address2 { get; set; }
+        [Column("acc_address_l3")]
+        public string? Address3 { get; set; }
+        [Column("acc_address_l4")]
+        public string? MiddleInitial { get; set; }
+        [Column("acc_address_l5")]
+        public string? Address5 { get; set; }
+        [Column("acc_address_l6")]
+        public string? Address6 { get; set; }
+        [Column("acc_city")]
+        public string? City { get; set; }
+        [Column("acc_state")]
+        public string? State { get; set; }
+        [Column("acc_postal_code")]
+        public string? State { get; set; }
+        [Column("acc_country")]
+        public string? Country { get; set; }
+        [Column("acc_region")]
+        public string? Region { get; set; }
+        [Column("acc_keyword")]
+        public string? Keyword { get; set; }
+        [Column("acc_phone")]
+        public string? Phone { get; set; }
+        [Column("acc_mobile")]
+        public string? Mobile { get; set; }
+        [Column("acc_fax")]
+        public string? Fax { get; set; }
+        [Column("acc_email_address")]
+        public string? Email { get; set; }
+        [Column("acc_website_address")]
+        public string? Website { get; set; }
+        [Column("acc_primary_account")]
+        public string? PrimaryAccount { get; set; }
+        [Column("acc_entered_on")]
+        public DateTime? EnteredOn { get; set; }
+        [Column("acc_changed_on")]
+        public DateTime? ChangedOn { get; set; }
+    }
+}
--- EnterpriseODataApis/Models/Event.cs
+++ EnterpriseODataApis/Models/Event.cs
@@ -1,7 +1,87 @@
 ﻿namespace EnterpriseODataApis.Models
 {
+    [Serializable]
+    [Table("Account")]
     public class Event
     {
+        [Key]
+        [Column("evt_srl_id")]
+        public int Id { get; set; }
 
+        [Column("org_srl_id")]
+        public string Organization { get; set; }
+        [Column("evt_start_date")]
+        public DateTime StartDate { get; set; }
+        [Column("evt_end_date")]
+        public DateTime EndDate { get; set; }
+        [Column("evt_desc")]
+        public string Description { get; set; }
+        [Column("evt_designation")]
+        public string Designation { get; set; }
+        //todo: object
+        [Column("evt_class")]
+        public string Class { get; set; }
+        //todo: object
+        [Column("evt_category")]
+        public string Category { get; set; }
+        //todo: object
+        [Column("evt_status")]
+        public string Status { get; set; }
+        [Column("evt_legal_name")]
+        public string LegalName { get; set; }
+        [Column("evt_in_date")]
+        public string InDate { get; set; }
+        [Column("evt_out_date")]
+        public string OutDate { get; set; }
+        //todo: object
+        [Column("evt_coordinator")]
+        public string Coordinator { get; set; }
+        [Column("evt_sales_person")]
+        public string Salesperson { get; set; }
+        [Column("evt_entered_by")]
+        public string EnteredBy { get; set; }
+        [Column("evt_entered_on")]
+        public DateTime EnteredOn { get; set; }
+        [Column("evt_sensitivity")]
+        public string Sensitivity { get; set; }
+        //todo: object
+        [Column("evt_note")]
+        public string Note { get; set; }
+        [Column("evt_actual_cost")]
+        public int ActualCost { get; set; }
+        [Column("evt_forecast_attend")]
+        public int ForecastAttendance { get; set; }
+        [Column("ForecastCost")]
+        public int evt_forecast_cost { get; set; }
+        [Column("evt_ord_attend")]
+        public int OrderedAttendance { get; set; }
+        [Column("evt_actual_attend")]
+        public int ActualAttendance { get; set; }
+
+        [Column("evt_type")]
+        public string EventType { get; set; }
+        [Column("evt_on_site_office")]
+        public string OnSiteOffice { get; set; }
+        [Column("evt_on_site_phone")]
+        public string OnSitePhone { get; set; }
+        [Column("evt_cust_number")]
+        //todo: object
+        public Customer Customer { get; set; }
+        [Column("evt_bill_to")]
+        public string BillTo { get; set; }
+        [Column("evt_web_address")]
+        public string WebAddress { get; set; }
+        [Column("evt_parent_srl_id")]
+        public int ParentEvent { get; set; }
+        [Column("evt_previous_srl_id")]
+        public int PreviousEvent { get; set; }
+        [Column("evt_actual_revenue")]
+        public int ActualRevenue { get; set; }
+        [Column("evt_ord_revenue")]
+        public int OrderedRevenue { get; set; }
+        [Column("evt_forecast_revenue")]
+        public int ForecastRevenue { get; set; }
+        [Column("evt_days")]
+        public int EventDays { get; set; }
     }
 }
